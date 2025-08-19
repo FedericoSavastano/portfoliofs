896ae505bdf28fe60de96a4e9cdb6cd144d3788b
FedericoSavastano <f.savastano@hotmail.com>
2025-08-19 18:05:12 -0300

    new test

--- src/data/works.js
+++ src/data/works.js
@@ -1,66 +1,66 @@
 export function works() {
-    return [
-        {
-            id: 1,
-            title: 'Boys Pet Shop',
-            urlName: 'boyspetshop',
-            description:
-                'CRUD application done with React and Typescript in the Front End, and Node with Express and PostgreSQL database in the Back End. ',
+  return [
+    {
+      id: 1,
+      title: "Boys Pet Shop",
+      urlName: "boyspetshop",
+      description:
+        "CRUD application done with React and Typescript in the Front End, and Node with Express and PostgreSQL database in the Back End. ",
 
-            tech: [
-                'React',
-                'Node',
-                'Express',
-                'Typescript',
-                'PostgreSQL',
-                'Tailwind',
-            ],
-            liveDemo: 'https://boyspetshop.vercel.app/',
-            gitHub: 'https://github.com/FedericoSavastano/boyspetshop_backend',
-            screenshot:
-                'https://res.cloudinary.com/dd1gptapb/image/upload/v1741970135/portfolio/boyspetshop_rwjvtx.jpg',
-            // flowchart: 'https://example.com/demo1',
-            docs: 'https://boyspetshop-backend.onrender.com/docs/',
-        },
-        {
-            id: 2,
-            title: 'Job 42',
-            urlName: 'job42',
-            description:
-                'This is job 42 according to the hitchhicker´s guide to the galaxy lorem consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation',
+      tech: [
+        "React",
+        "Node",
+        "Express",
+        "Typescript",
+        "PostgreSQL",
+        "Tailwind",
+      ],
+      liveDemo: "https://boyspetshop.vercel.app/",
+      gitHub: "https://github.com/FedericoSavastano/boyspetshop_backend",
+      screenshot:
+        "https://res.cloudinary.com/dd1gptapb/image/upload/v1741970135/portfolio/boyspetshop_rwjvtx.jpg",
+      // flowchart: 'https://example.com/demo1',
+      docs: "https://boyspetshop-backend.onrender.com/docs/",
+    },
+    {
+      id: 2,
+      title: "Job 42",
+      urlName: "job42",
+      description:
+        "This is job 42 according to the hitchhicker´s guide to the galaxy lorem consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation",
 
-            tech: ['React', 'Styled-Components', 'Tailwind'],
-            liveDemo: 'https://example.com/demo1',
-            gitHub: 'https://github.com/',
-            screenshot:
-                'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png',
-            flowchart: 'https://example.com/demo1',
-        },
-        {
-            id: 3,
-            title: 'Job3',
-            urlName: 'job3',
-            description:
-                'This is job 3 consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation',
-            tech: ['React', 'React router dom'],
-            liveDemo: 'https://example.com/demo1',
-            gitHub: 'https://github.com/',
-            screenshot:
-                'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png',
-            flowchart: 'https://example.com/demo1',
-        },
-        {
-            id: 4,
-            title: 'Job4',
-            urlName: 'job4',
-            description:
-                'This is job 4 consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation',
-            tech: ['JavaScript', 'CSS', 'HTML', 'JavaScript1', 'CSS1', 'HTML1'],
-            liveDemo: 'https://example.com/demo1',
-            gitHub: 'https://github.com/',
-            screenshot:
-                'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png',
-            // flowchart: 'https://example.com/demo1',
-        },
-    ];
+      tech: ["React", "Styled-Components", "Tailwind"],
+      liveDemo: "https://example.com/demo1",
+      gitHub: "https://github.com/",
+      screenshot:
+        "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png",
+      flowchart: "https://example.com/demo1",
+    },
+    {
+      id: 3,
+      title: "Job XX",
+      urlName: "job3",
+      description:
+        "This is job 3 consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation",
+      tech: ["React", "React router dom"],
+      liveDemo: "https://example.com/demo1",
+      gitHub: "https://github.com/",
+      screenshot:
+        "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png",
+      flowchart: "https://example.com/demo1",
+    },
+    {
+      id: 4,
+      title: "Job4",
+      urlName: "job4",
+      description:
+        "This is job 4 consectetur adipisicing elit. Illum sequi, est nemo eveniet incidunt, commodi sed optio dolores id eum molestias perferendis exercitation",
+      tech: ["JavaScript", "CSS", "HTML", "JavaScript1", "CSS1", "HTML1"],
+      liveDemo: "https://example.com/demo1",
+      gitHub: "https://github.com/",
+      screenshot:
+        "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png",
+      // flowchart: 'https://example.com/demo1',
+    },
+  ];
 }
