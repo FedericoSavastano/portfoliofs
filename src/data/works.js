4be416e54ba59935fe18691934457f076ac98b80
FedericoSavastano <f.savastano@hotmail.com>
2025-08-22 19:01:48 -0300

    taskly docs

--- src/data/works.js
+++ src/data/works.js
@@ -4,9 +4,27 @@ export function works() {
       id: 2,
       title: "Taskly - Task manager",
       urlName: "taskly",
-      description: "Task managing fullstack application. You can create and ",
+      description:
+        "Taskly is a fullstack task management app built with the MERN stack. It allows users to create projects, assign roles, manage tasks with status updates, and add notes. Developed with React + TypeScript on the frontend and Node.js, Express, and MongoDB (Mongoose) with TypeScript on the backend.",
 
-      fullDescription: "",
+      fullDescription: `Taskly is a fullstack project management application inspired by tools like Trello, designed to streamline team collaboration and task organization.
+
+The platform provides user authentication and account creation, ensuring secure access to projects. Once logged in, users can:
+
+Create and manage projects
+
+Assign roles and tasks to team members
+
+Track task progress by updating their status
+
+Add notes and comments directly to tasks for better context and communication
+
+From a technical perspective, the frontend was built with React and TypeScript, delivering a responsive and type-safe user experience. The backend was developed using Node.js, Express, and MongoDB (Mongoose) with TypeScript, enabling scalable data management and a robust API layer.
+
+This project demonstrates not only technical proficiency in the MERN stack with TypeScript but also the ability to design and implement real-world features for collaborative productivity tools.
+Feel free to create your own user, or if you want to quickly look at the app you can enter as federicosavastano.dev@gmail.com , password: tasklytester
+
+`,
       tech: [
         "React",
         "React router dom",
@@ -17,7 +35,6 @@ export function works() {
         "Mongoose",
         "TailwindCSS",
         "Zod",
-        "",
       ],
       liveDemo: "https://taskly-taskmanager.vercel.app/",
       gitHub: "https://github.com/FedericoSavastano/taskly_backend",
