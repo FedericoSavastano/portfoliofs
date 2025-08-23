42ef4e5b0091af4d00a158ab42dfa1050be8f4f6
FedericoSavastano <f.savastano@hotmail.com>
2025-08-22 22:06:05 -0300

    better work

--- src/data/works.js
+++ src/data/works.js
@@ -8,29 +8,22 @@ export function works() {
         "Taskly is a fullstack task management app built with the MERN stack. It allows users to create projects, assign roles, manage tasks with status updates, and add notes. Developed with React + TypeScript on the frontend and Node.js, Express, and MongoDB (Mongoose) with TypeScript on the backend.",
 
       fullDescription: `
-      <p>Taskly is a fullstack project management application inspired by tools like Trello, designed to streamline team collaboration and task organization.
+Taskly is a fullstack project management application inspired by tools like Trello, designed to streamline team collaboration and task organization.
 
-The platform provides user authentication and account creation, ensuring secure access to projects. </p>
-<p>Once logged in, users can: </p>
+The platform provides user authentication and account creation, ensuring secure access to projects. Once logged in, users can:
+- Create and manage projects
+- Assign roles and tasks to team members
+- Track task progress by updating their status
+- Add notes and comments directly to tasks for better context and communication
 
-<ul>
-<li>Create and manage projects</li>
+From a technical perspective, the frontend was built with React and TypeScript, delivering a responsive and type-safe user experience. The backend was developed using Node.js, Express, and MongoDB (Mongoose) with TypeScript, enabling scalable data management and a robust API layer.
+This project demonstrates not only technical proficiency in the MERN stack with TypeScript but also the ability to design and implement real-world features for collaborative productivity tools.
 
-<li>Assign roles and tasks to team members</li>
-
-<li>Track task progress by updating their status</li>
-
-<li>Add notes and comments directly to tasks for better context and communication</li>
-
-</ul>
-
-<p>fFrom a technical perspective, the frontend was built with React and TypeScript, delivering a responsive and type-safe user experience. The backend was developed using Node.js, Express, and MongoDB (Mongoose) with TypeScript, enabling scalable data management and a robust API layer.
-
-This project demonstrates not only technical proficiency in the MERN stack with TypeScript but also the ability to design and implement real-world features for collaborative productivity tools.</p>
-<p>fFeel free to create your own user, or if you want to quickly look at the app you can enter as</p>
- <p>federicosavastano.dev@gmail.com , password: tasklytester</p>
-
+Feel free to create your own user or if you want to quickly check the app sign in as:
+- user: federicosavastano.dev@gmail.com
+- pass: tasklytester
 `,
+
       tech: [
         "React",
         "React router dom",
@@ -54,9 +47,25 @@ This project demonstrates not only technical proficiency in the MERN stack with 
       id: 3,
       title: "McDowell's Burgers and Café",
       urlName: "mcdowells",
-      description: "Task managing fullstack application. You can create and ",
+      description:
+        "McDowell’s is a fullstack app built with Next.js and TypeScript that simulates a fast-food kiosk. Customers can place and pay for orders, while staff manage them through an admin dashboard. Completed orders are displayed on a pickup screen. It uses MongoDB with Prisma, SWR, Zod, and Zustand for data handling and validation.",
 
-      fullDescription: "",
+      fullDescription: `
+McDowell’s is a fullstack application developed with Next.js and TypeScript, designed to function as a kiosk system for a fast-food and café business.
+
+* The customer-facing side runs under /order/[category], where users can select items, place an order, and complete payment seamlessly.
+
+* The kitchen/admin panel is located at /admin, where staff can:
+      -- View incoming orders in real-time
+      -- Update order statuses (e.g., mark as completed)
+      -- Manage the menu by creating and editing products
+
+* Once an order is completed, it is displayed at /orders, serving as the pickup screen where customers can confirm their orders are ready.
+
+The app is powered by MongoDB with Prisma for database management, and uses SWR, Zod, and Zustand for state management, data fetching, and schema validation. Together, these tools ensure a responsive, scalable, and reliable kiosk experience tailored for the food-service industry.
+
+Feel free to create your own orders!
+`,
       tech: [
         "React",
         "NextJs",
@@ -70,27 +79,42 @@ This project demonstrates not only technical proficiency in the MERN stack with 
         "Zustand",
         "SWR",
       ],
-      liveDemo: "https://taskly-taskmanager.vercel.app/",
-      gitHub: "https://github.com/FedericoSavastano/taskly_backend",
+      liveDemo: "https://mcdowells-nextjs-shop.vercel.app/",
+      adminPanel: "https://mcdowells-nextjs-shop.vercel.app/admin/orders",
+      orders: "https://mcdowells-nextjs-shop.vercel.app/orders",
+      gitHub: "https://github.com/FedericoSavastano/mcdowells_nextjs_shop",
       screenshot:
         "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210401151214/What-is-Website.png",
       //flowchart: "https://example.com/demo1",
-      docs: "https://taskly-backend-8sam.onrender.com/docs",
+      // docs: "https://taskly-backend-8sam.onrender.com/docs",
     },
     {
       id: 1,
       title: "Boys Pet Shop",
       urlName: "boyspetshop",
       description:
-        "CRUD application made with React and Typescript in the Front End, and Node with Express and PostgreSQL database in the Back End. ",
+        "Boys Pet Shop is a fullstack CRUD application built with React, TypeScript, Node.js, Express, and PostgreSQL. It allows seamless management of pet products, clean architecture and strong integration between the frontend and backend.",
 
-      fullDescription: "",
+      fullDescription: `
+Boys Pet Shop is a fullstack CRUD (Create, Read, Update, Delete) application designed to manage a pet shop’s core data and operations.
+
+* The frontend, developed with React and TypeScript, provides a clean and responsive interface for handling pet shop products.
+
+* The backend, built with Node.js and Express, connects to a PostgreSQL database, ensuring reliable and efficient data storage.
+
+* Core features include adding, editing, viewing, and deleting records, with a strong focus on data integrity and user experience.
+
+This project highlights fullstack development skills, database modeling, and the ability to build scalable, maintainable CRUD applications that serve as the foundation for real-world business solutions.
+
+Feel free to create your own pet products!
+`,
       tech: [
         "React",
         "Node",
         "Express",
         "TypeScript",
         "PostgreSQL",
+        "React router dom",
         "Tailwind",
       ],
       liveDemo: "https://boyspetshop.vercel.app/",
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -13,64 +13,30 @@ export async function loader({ params }) {
 }
 
 const Section = styled.div`
-  height: 100vh;
   padding: 0% 1.5%;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
+  min-height: 100dvh;
   @media only screen and (max-width: 768px) {
-    min-height: fit-content;
-    align-items: center;
-    justify-content: space-evenly;
   }
 `;
 
-const Container = styled.div`
-  width: 100%;
-  height: 80%;
+const ImageAndDescription = styled.div`
   display: flex;
-  justify-content: space-between;
-  gap: 2rem;
+  flex-direction: row;
+  margin: 2rem;
 
   @media only screen and (max-width: 768px) {
-    height: fit-content;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-
-  @media only screen and (max-width: 768px) {
-    align-items: center;
-    text-align: center;
-    width: 100%;
   }
 `;
 
 const Title = styled.h2`
   text-shadow: 1.2px 1.2px 2px black;
-  align-self: flex-start;
-
-  @media only screen and (max-width: 768px) {
-    align-self: center;
-  }
+  text-align: center;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
 
 const Img = styled.img`
@@ -82,7 +48,7 @@ const Img = styled.img`
   }
 `;
 
-const Button = styled.button`
+const ButtonBack = styled.button`
   background-color: ${styles.color.turquoise};
   color: white;
   border: none;
@@ -93,9 +59,10 @@ const Button = styled.button`
   box-shadow: 10px 5px 5px black;
   font-family: ${styles.fontMain};
   text-shadow: 0.8px 0.8px 2px black;
+  margin: 3rem 0rem 4rem 0rem;
 `;
 
-const Button2 = styled.button`
+const Button = styled.button`
   background-color: ${styles.color.bubblegum};
   color: white;
   border: none;
@@ -106,18 +73,30 @@ const Button2 = styled.button`
   box-shadow: 10px 5px 5px black;
   font-family: ${styles.fontMain};
   text-shadow: 0.8px 0.8px 2px black;
+  width: 10em;
 `;
 
 const Buttons = styled.div`
   display: flex;
-  justify-content: space-around;
+  flex-direction: row;
+  justify-content: center;
   width: 100%;
-  align-items: start;
+  align-items: center;
+  gap: 2rem;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const Desc = styled.div`
   margin-bottom: 2rem;
+  max-width: 80vw;
+  display: flex;
+  justify-content: center;
   text-shadow: 0.8px 0.8px 2px black;
+  white-space: pre-line;
+  font-weight: 500;
 `;
 
 const Tech = styled.p`
@@ -125,10 +104,7 @@ const Tech = styled.p`
   color: rgba(0, 255, 238, 1);
   font-style: italic;
   text-shadow: 0.8px 0.8px 2px black;
-`;
-
-const BackButtonWrapper = styled.div`
-  margin: 1rem;
+  text-align: center;
 `;
 
 const ErrorWrapper = styled.div`
@@ -155,91 +131,85 @@ function Work() {
 
   return work ? (
     <Section>
-      <Container>
-        <Left>
-          <Title>{work.title}</Title>
+      <Title>{work.title}</Title>
+      <Tech>{work.tech.join(" - ")}</Tech>
+      <ImageAndDescription>
+        <Wrapper>
           <Img src={work.screenshot}></Img>
-        </Left>
-        <Right>
-          <Desc> {work.fullDescription} </Desc>
-          <Tech>{work.tech.join(" - ")}</Tech>
+        </Wrapper>
+        <Wrapper>
+          <Desc>{work.fullDescription}</Desc>
+        </Wrapper>
+      </ImageAndDescription>
 
-          <Buttons>
-            {work.flowchart && (
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                to={work.flowchart}
+      <Buttons>
+        {/* Demo */}
+        <Link target="_blank" rel="noopener noreferrer" to={work.liveDemo}>
+          <Button>
+            <Icon>
+              <IconSvg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 640 512"
               >
-                <Button2>
-                  <Icon>
-                    <IconSvg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="1em"
-                      viewBox="0 0 512 512"
-                    >
-                      <path d="M224 32H64C46.3 32 32 46.3 32 64v64c0 17.7 14.3 32 32 32H441.4c4.2 0 8.3-1.7 11.3-4.7l48-48c6.2-6.2 6.2-16.4 0-22.6l-48-48c-3-3-7.1-4.7-11.3-4.7H288c0-17.7-14.3-32-32-32s-32 14.3-32 32zM480 256c0-17.7-14.3-32-32-32H288V192H224v32H70.6c-4.2 0-8.3 1.7-11.3 4.7l-48 48c-6.2 6.2-6.2 16.4 0 22.6l48 48c3 3 7.1 4.7 11.3 4.7H448c17.7 0 32-14.3 32-32V256zM288 480V384H224v96c0 17.7 14.3 32 32 32s32-14.3 32-32z" />
-                    </IconSvg>
-                  </Icon>
-                  {texts.workButtonFlowChart}
-                </Button2>
-              </Link>
-            )}
+                <path d="M64 96c0-35.3 28.7-64 64-64H512c35.3 0 64 28.7 64 64V352H512V96H128V352H64V96zM0 403.2C0 392.6 8.6 384 19.2 384H620.8c10.6 0 19.2 8.6 19.2 19.2c0 42.4-34.4 76.8-76.8 76.8H76.8C34.4 480 0 445.6 0 403.2zM281 209l-31 31 31 31c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-9.4-9.4-9.4-24.6 0-33.9l48-48c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM393 175l48 48c9.4 9.4 9.4 24.6 0 33.9l-48 48c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l31-31-31-31c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z" />
+              </IconSvg>
+            </Icon>
+            {texts.workButtonLiveDemo}
+          </Button>
+        </Link>
 
-            <Link target="_blank" rel="noopener noreferrer" to={work.liveDemo}>
-              <Button2>
-                <Icon>
-                  <IconSvg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1em"
-                    viewBox="0 0 640 512"
-                  >
-                    <path d="M64 96c0-35.3 28.7-64 64-64H512c35.3 0 64 28.7 64 64V352H512V96H128V352H64V96zM0 403.2C0 392.6 8.6 384 19.2 384H620.8c10.6 0 19.2 8.6 19.2 19.2c0 42.4-34.4 76.8-76.8 76.8H76.8C34.4 480 0 445.6 0 403.2zM281 209l-31 31 31 31c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-9.4-9.4-9.4-24.6 0-33.9l48-48c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM393 175l48 48c9.4 9.4 9.4 24.6 0 33.9l-48 48c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l31-31-31-31c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z" />
-                  </IconSvg>
-                </Icon>
-                {texts.workButtonLiveDemo}
-              </Button2>
-            </Link>
+        {work.adminPanel && (
+          <Link target="_blank" rel="noopener noreferrer" to={work.adminPanel}>
+            <Button>Admin panel</Button>
+          </Link>
+        )}
 
-            <Link target="_blank" rel="noopener noreferrer" to={work.gitHub}>
-              <Button2>
-                <Icon>
-                  <IconSvg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1em"
-                    viewBox="0 0 496 512"
-                  >
-                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                  </IconSvg>
-                </Icon>
-                {texts.workButtonGitHub}
-              </Button2>
-            </Link>
+        {work.orders && (
+          <Link target="_blank" rel="noopener noreferrer" to={work.orders}>
+            <Button>Orders</Button>
+          </Link>
+        )}
 
-            {work.docs && (
-              <Link target="_blank" rel="noopener noreferrer" to={work.docs}>
-                <Button2>
-                  <Icon>
-                    <IconSvg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="1em"
-                      viewBox="0 0 512 512"
-                    >
-                      <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z" />
-                    </IconSvg>
-                  </Icon>
-                  {texts.docsButton}
-                </Button2>
-              </Link>
-            )}
-          </Buttons>
-        </Right>
-      </Container>
+        {/* Github */}
+        <Link target="_blank" rel="noopener noreferrer" to={work.gitHub}>
+          <Button>
+            <Icon>
+              <IconSvg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 496 512"
+              >
+                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+              </IconSvg>
+            </Icon>
+            {texts.workButtonGitHub}
+          </Button>
+        </Link>
 
-      <BackButtonWrapper>
+        {/* Documentation */}
+        {work.docs && (
+          <Link target="_blank" rel="noopener noreferrer" to={work.docs}>
+            <Button>
+              <Icon>
+                <IconSvg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z" />
+                </IconSvg>
+              </Icon>
+              {texts.docsButton}
+            </Button>
+          </Link>
+        )}
+      </Buttons>
+
+      <Buttons>
         <Link to={"/"}>
           {" "}
-          <Button>
+          <ButtonBack>
             <Icon>
               <IconSvg
                 xmlns="http://www.w3.org/2000/svg"
@@ -251,15 +221,15 @@ function Work() {
               </IconSvg>
             </Icon>
             {texts.workButtonBack}
-          </Button>
+          </ButtonBack>
         </Link>
-      </BackButtonWrapper>
+      </Buttons>
     </Section>
   ) : (
     <Section>
       <ErrorWrapper>
         <Title>{texts.workNonExistant}</Title>
-        <BackButtonWrapper>
+        <Buttons>
           <Link to={"/"}>
             {" "}
             <Button>
@@ -276,7 +246,7 @@ function Work() {
               {texts.workButtonBack}
             </Button>
           </Link>
-        </BackButtonWrapper>
+        </Buttons>
       </ErrorWrapper>
     </Section>
   );
